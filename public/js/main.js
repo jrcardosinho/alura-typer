c394e5770d87d39e42093ea0128579024949a74c
Luiz Alberto de Freitas Cardoso Junior <jr.cardosinho@gmail.com>
2018-03-22 16:21:20 -0300

    Fim da Aula 4
    Código como ficou após a conclusão da aula 4.

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,53 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
+var tempoInicial = $("#tempoDigitacao").text();
 
-var tamFrase = $("#tamFrase");
-tamFrase.text(numPalavras);
+
+$(function(){ //equivale a: $(document).ready(function()
+    inicializaFrase();
+    inicializaContadores();
+    inicializaCronometro();
+});
+
+function inicializaFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamFrase = $("#tamFrase");
+    tamFrase.text(numPalavras);
+}
 
 var campo = $(".campo-digitacao");
+
+function inicializaContadores(){
 campo.on("input",function(){
     var conteudo = campo.val();
     var qtdPalavras = conteudo.split(/\S+/).length - 1;
     $("#contPalavras").text(qtdPalavras);
     $("#contCaracteres").text(conteudo.length);
+    $("#botaoReiniciar").click(reiniciaJogo);
 });
+}
 
-var tempoRestante = $("#tempoDigitacao").text();
-
+function inicializaCronometro() {
 campo.one("focus", function(){
+    var tempoRestante = $("#tempoDigitacao").text();
+    $("#botaoReiniciar").attr("disabled", true);
     var cronometroID = setInterval(function(){
         tempoRestante--;
         $("#tempoDigitacao").text(tempoRestante);
         if(tempoRestante < 1) {
             campo.attr("disabled", true);
             clearInterval(cronometroID);
+            $("#botaoReiniciar").attr("disabled", false);
         };
     },1000);
 });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contPalavras").text("0");
+    $("#contCaracteres").text("0");
+    $("#tempoDigitacao").text(tempoInicial);
+    inicializaCronometro();
+}
+
